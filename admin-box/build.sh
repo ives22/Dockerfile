1205193e1a3ea66b7e3f4f749d6a74e1056eaf08
jalen <jalen00688@gmail.com>
2026-01-08 10:54:10 +0800

    update admin-box

--- admin-box/build.sh
+++ admin-box/build.sh
@@ -1,1 +1,1 @@
-docker buildx build -t vvoo/admin-box:v1.0  --platform=linux/amd64,linux/arm64,linux/arm . --push
+docker buildx build -t vvoo/admin-box:latest  --platform=linux/amd64,linux/arm64,linux/arm . --push
